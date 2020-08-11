2422f6107179b4bbc06442a6198a6ac2603fa95f
Veesh Goldman <rabbiveesh@gmail.com>
2020-08-12 00:19:00 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test2::Tools::TypeTiny - It's new $module
+
+# SYNOPSIS
+
+    use Test2::Tools::TypeTiny;
+
+# DESCRIPTION
+
+Test2::Tools::TypeTiny is ...
+
+# LICENSE
+
+Copyright (C) Veesh Goldman.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Veesh Goldman <rabbiveesh@gmail.com>
---
+++ lib/Test2/Tools/TypeTiny.pm
@@ -0,0 +1,46 @@
+package Test2::Tools::TypeTiny;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+use Exporter::Shiny qw/type_tiny/;
+
+sub type_tiny {
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test2::Tools::TypeTiny - Use Type::Tiny in your Test2 Tests!
+
+=head1 SYNOPSIS
+
+    use Test2::Tools::TypeTiny;
+    use Types::Standard -all;
+
+    is 1, type Int, 
+
+=head1 DESCRIPTION
+
+TODO-
+Test2::Tools::TypeTiny is  
+
+=head1 LICENSE
+
+Copyright (C) Veesh Goldman.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Veesh Goldman E<lt>rabbiveesh@gmail.comE<gt>
+
+=cut
+
